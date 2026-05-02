d68db156f418e26185ab41e7a15e97bc71aeab20
Dylan Gorgioski <dylan.gorgio@gmail.com>
2026-05-02 11:18:23 +1000

    [hot-fix] added react import to App.jsx and main.jsx to functional webpage that displays contents

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,10 @@
 import 'meteor-node-stubs';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './main.css';
 import { App } from '../imports/ui/App';
 
-createRoot(document.getElementById('app')).render(<App />);
+Meteor.startup(() => {
+  createRoot(document.getElementById('app')).render(<App />);
+});
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
