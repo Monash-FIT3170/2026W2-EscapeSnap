ad3593d08e09012c7073d4fe7925e6be44ed2413
Dylan Gorgioski <dylan.gorgio@gmail.com>
2026-05-02 20:37:30 +1000

    Added initial files for game creation screen

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router';
 
 import Dashboard from './host/pages/dashboard/Dashboard';
 import FinalRiddlePage from './host/pages/riddle/FinalRiddlePage';
+import CreateGame from './host/pages/create-game/CreateGame';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/game/create" element={<CreateGame />} />
       <Route path="/game/:gameId/final-riddle" element={<FinalRiddlePage />} />
     </Routes>
   );
