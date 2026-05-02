d0e17c31f7dbe550412ce59148102b9c4ecafb5d
Dylan Gorgioski <dylan.gorgio@gmail.com>
2026-05-02 13:39:22 +1000

    fixed key issue in the revealed letters card.

--- imports/ui/host/components/riddle/RevealedLetters.jsx
+++ imports/ui/host/components/riddle/RevealedLetters.jsx
@@ -6,9 +6,9 @@ const RevealedLetters = ({ letters }) => {
       {letters.map((letter, index) => (
 
       
-        <div className="hover-3d">
+        <div key={index}  className="hover-3d">
         {/* content */}
-        <div key={index} className="w-24 h-24 flex items-center justify-center border-b-2 border-red-600 bg-gray-600 text-white font-bold text-2xl uppercase tracking-widest">
+        <div className="w-24 h-24 flex items-center justify-center border-b-2 border-red-600 bg-gray-600 text-white font-bold text-2xl uppercase tracking-widest">
           {letter}
         </div>
         {/* 8 empty divs needed for the 3D effect */}
