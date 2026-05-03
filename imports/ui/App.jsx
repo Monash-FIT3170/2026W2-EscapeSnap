d08dfbf61aa72318fbd0067f2b84bf21c60d13df
Udayan Mishra <udayangem3@gmail.com>
2026-05-04 00:07:03 +1000

    Updated routing to point root URL to landing page

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
-
+import Landing from './host/pages/landing/Landing';
 import Dashboard from './host/pages/dashboard/Dashboard';
 import FinalRiddlePage from './host/pages/riddle/FinalRiddlePage';
 import CreateGame from './host/pages/create-game/CreateGame';
@@ -9,6 +9,7 @@ import Lobby from './host/pages/lobby/Lobby';
 export function App() {
   return (
     <Routes>
+      <Route path="/" element={<Landing />} />
       <Route path="/" element={<Dashboard />} />
       <Route path="/game/create" element={<CreateGame />} />
       <Route path="/game/:gameId/lobby" element={<Lobby />} />
